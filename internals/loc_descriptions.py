58e29f4300dea464bd150139544df8689c26f7c7
Alex Sun <alexsun82@icloud.com>
2020-01-18 02:21:21 +0100

    Added file for location descriptions

---
+++ internals/loc_descriptions.py
@@ -0,0 +1,3 @@
+"""File containing location description strings."""
+
+loc_main_street_start = "You are standing in the middle of an empty street. It should not be this empty. Looking at your watch, you realize that it is still early. No need to go home yet."
