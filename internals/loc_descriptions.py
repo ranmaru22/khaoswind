eaf3769e86a85dd7c81688237b7d4154211389a6
Alex Sun <alexsun82@icloud.com>
2020-01-18 18:01:29 +0100

    Changed location desc. file to JSON format

--- internals/loc_descriptions.py
+++
@@ -1,3 +0,0 @@
-"""File containing location description strings."""
-
-loc_main_street_start = "You are standing in the middle of an empty street. It should not be this empty. Looking at your watch, you realize that it is still early. No need to go home yet."
