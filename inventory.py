a4f65823d579f09ea83494cd5118dea67720d71c
Alex Sun <alexsun82@icloud.com>
2020-01-19 02:03:34 +0100

    Added inventory feature.

---
+++ inventory.py
@@ -0,0 +1,43 @@
+"""This file handles the player's inventory."""
+
+
+class Inventory(object):
+    """Base class for the inventory object."""
+    
+    def __init__(self):
+        self.items = []
+        
+    def add(self, item_obj, location_obj, stack):
+        """Adds an item to the player's inventory, removes it from the location
+        and pushes a message to the stack.
+        If the location is None, the item is added covertly (i.e no message is
+        pushed onto the stack).
+        """
+        self.items.append(item_obj)
+        if location_obj is not None:
+            del location_obj.items[item_obj.name]
+            stack.append(f"You pick up the {item_obj.name}.")
+            
+    def check(self):
+        """Executes when the player enters the inv command."""
+        if len(self.items) == 1:
+            text = f"You have a {next(iter(self.items)).name}."
+        # If there is more than one item, return a string that lists all the
+        # items in a comma-separated sentence.
+        elif len(self.items) > 1:
+            first = True
+            text = str()
+            for item in self.items:
+                if first:
+                    item_str += f"You have  a {item.name}"
+                    first = False
+                else:
+                    item_str += f", a {item.name}"
+            else:
+                item_str = item_str.rsplit(
+                    ', a', 1)[0] + ', and a' + item_str.rsplit(', a', 1)[-1] + '.'
+        # Catch-all clause if inventory is empty.
+        else:
+            text = "You don't carry anything."
+        return text
+            
---
+++ items.py
@@ -0,0 +1,39 @@
+"""The items of the game."""
+
+import json
+
+# Load the item descriptions.
+with open("internals/item_descriptions.json") as f_obj:
+    item_descriptions = json.load(f_obj)
+
+
+class Item(object):
+    """Template class for items."""
+
+    def __init__(self, name, allow_pickup, location):
+        self.name = name
+        self.status = 0
+        self.allow_pickup = allow_pickup
+        self.location = location
+
+        # Upon creation, the new location object will automatically
+        # pull the base description from loc_descriptions.json.
+        self.description = None
+        self._get_desc()
+
+        # Add it to the location
+        self._add_to_loc(location)
+
+    def _get_desc(self):
+        """Gets the items's description text from item_descriptions.json."""
+        self.description = item_descriptions[self.name][self.status]
+
+    def _add_to_loc(self, location):
+        """Adds the item to the location's inventory."""
+        location.add_item(self.name, self)
+
+    def pick_up(self, inventory, location, stack):
+        """Adds the item to the player's inventory and removes it from the
+        location's inventory.
+        """
+        inventory.add(self)
