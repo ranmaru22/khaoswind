0d82b7cc53ecf15c127550f66789fdf5e80260ce
Alex Sun <alexsun82@icloud.com>
2020-01-19 21:56:35 +0100

    Fixed a typo.

--- inventory.py
+++ inventory.py
@@ -3,10 +3,10 @@
 
 class Inventory(object):
     """Base class for the inventory object."""
-    
+
     def __init__(self):
         self.items = []
-        
+
     def add(self, item_obj, location_obj, stack):
         """Adds an item to the player's inventory, removes it from the location
         and pushes a message to the stack.
@@ -17,20 +17,20 @@ class Inventory(object):
         if location_obj is not None:
             del location_obj.items[item_obj.name]
             stack.append(f"You pick up the {item_obj.name}.")
-            
+
     def check(self):
         """Executes when the player enters the inv command."""
         if len(self.items) == 1:
-            text = f"You have a {next(iter(self.items)).name}."
+            text = f"You carry a {next(iter(self.items)).name}."
         # If there is more than one item, return a string that lists all the
         # items in a comma-separated sentence.
         elif len(self.items) > 1:
-            first = True
+            is_first = True
             text = str()
             for item in self.items:
-                if first:
-                    item_str += f"You have  a {item.name}"
-                    first = False
+                if is_first:
+                    item_str += f"You carry a {item.name}"
+                    is_first = False
                 else:
                     item_str += f", a {item.name}"
             else:
@@ -40,4 +40,3 @@ class Inventory(object):
         else:
             text = "You don't carry anything."
         return text
-            
