355b8f9ec0d58ca7de769b74875d3b98a66078e2
Alex Sun <alexsun82@icloud.com>
2020-01-26 23:12:35 +0100

    Fixed a bug with the map.

--- game_functions.py
+++ game_functions.py
@@ -12,18 +12,17 @@ def print_speak(text):
         time.sleep(0.025)
 
 
+# @debug
 def draw_map(current_loc, loc_map):
     """Draws a map based on a list of RG locations."""
     coordinates = [(loc.x, loc.y) for loc in loc_map]
-    print(coordinates)
+    print(coordinates, end='')
 
     for y in range(5, -5, -1):
         print()
         for x in range(-9, 9):
             if (x, y) == (current_loc.x, current_loc.y):
                 print('X', end='')
-            elif (x, y) == (0, 0):
-                print('0', end='')
             elif (x, y) in coordinates:
                 print('#', end='')
             else:
