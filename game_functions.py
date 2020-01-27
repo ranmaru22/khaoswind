2e72a7343ce3593e805e24a8bcc3b534b002aad3
Alex Sun <alexsun82@icloud.com>
2020-01-27 17:28:13 +0100

    Removed useless whitespace

--- game_functions.py
+++ game_functions.py
@@ -17,7 +17,6 @@ def draw_map(current_loc, loc_map):
     """Draws a map based on a list of RG locations."""
     coordinates = [(loc.x, loc.y) for loc in loc_map]
     print(coordinates, end='')
-
     for y in range(5, -5, -1):
         print()
         for x in range(-9, 9):
